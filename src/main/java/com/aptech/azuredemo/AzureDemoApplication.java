3da0d2cff30ecedb3f49e4aab6ff62026f1e2b7c
anuppachunkar <anuppachunkar@gmail.com>
2025-07-08 11:52:23 +0530

    message api

--- src/main/java/com/aptech/azuredemo/AzureDemoApplication.java
+++ src/main/java/com/aptech/azuredemo/AzureDemoApplication.java
@@ -2,9 +2,17 @@ package com.aptech.azuredemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class AzureDemoApplication {
+
+    @GetMapping("/message")
+    public String message(){
+        return "My First Spring Boot Application with azure";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AzureDemoApplication.class, args);
